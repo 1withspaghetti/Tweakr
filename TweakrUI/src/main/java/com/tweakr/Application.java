f42e5efebced6d59cc136760ab4f6f14a8570f3b
Populist <81264994+williamdelsea@users.noreply.github.com>
2024-05-14 16:04:43 -0700

    trying to figure out how to display image

--- TweakrUI/src/main/java/com/tweakr/Application.java
+++ TweakrUI/src/main/java/com/tweakr/Application.java
@@ -1,33 +1,63 @@
 package com.tweakr;
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
 
 public class Application extends JFrame {
 
     JFileChooser fileChooser;
     JPanel panel;
+    JTextArea text;
     JButton fileButton;
+    JButton exitButton;
+    File imagePath;
+    ImageIcon currentIcon = new ImageIcon("C:\\Users\\willi\\OneDrive\\probably very important stuff\\Pictures\\Camera Roll\\captures and misc\\discord pfp");
+    JLabel image;
 
-
-    public Application() {
+    public Application() throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
 
         super("Tweakr");
 
-        fileChooser = new JFileChooser("C:");
-        panel = new JPanel();
-        fileButton = new JButton("Choose files");
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(true);
+        setResizable(false);
         setSize(512, 512);
 
+
+        fileChooser = new JFileChooser(System.getProperty("user.dir"));
+        panel = new JPanel();
+        fileButton = new JButton("Choose files");
+        text = new JTextArea("haiii",1, 10);
+        exitButton = new JButton("Exit");
+        //image = new JLabel()
+
+        //fileButton.setlo
+        setLayout(new GridBagLayout());
+
         panel.add(fileButton);
+        //panel.add(text);
+        panel.add(exitButton);
+        //panel.setLayout(new )
         add(panel);
         setVisible(true);
 
         fileButton.addActionListener(e -> {
-            fileChooser.showSaveDialog(null);
+            //fileChooser.showSaveDialog(null);
+            //image = new JLabel()
+            System.out.println("A");
+            panel.add(new JButton("jdiejdjiw"));
+            System.out.println("B");
+        });
+
+        exitButton.addActionListener(e -> {
+            //dispatchEvent(new WindowEvent(Application, WindowEvent.WINDOW_CLOSED));
+            dispose();
         });
     }
 }
--- TweakrUI/src/main/java/com/tweakr/Main.java
+++ TweakrUI/src/main/java/com/tweakr/Main.java
@@ -3,7 +3,7 @@ package com.tweakr;
 import javax.swing.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
 
         Application app = new Application();
     }
