08dded9aa57954c05b9adea70c61e65c314fc589
Populist <81264994+williamdelsea@users.noreply.github.com>
2024-05-17 14:08:52 -0700

    renamed project file for clarity

---
+++ TweakrClient/src/main/java/com/tweakr/Main.java
@@ -0,0 +1,10 @@
+package com.tweakr;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+        Application app = new Application();
+    }
+}
